d32eff3decb5efbe10aecd7188127e57b50f1ca4
eggplantiny <eggplantiny@gmail.com>
2020-09-01 18:26:40 +0900

    test

--- frontend/pages/Library2/index.vue
+++ frontend/pages/Library2/index.vue
@@ -14,17 +14,17 @@
                             elevation="0"
                         >
                             <v-row dense>
-                                <v-col cols="4">
+                                <v-col cols="3">
                                     <v-img
                                         :src="article.book.thumbnail"
                                         aspect-ratio="0.66"
                                         max-width="140"
                                         width="100%"
-                                        class="book-border mx-2 my-1"
+                                        class="book-border ml-1"
                                     />
                                 </v-col>
-                                <v-col cols="8">
-                                    <v-card-title class="border justify-center">
+                                <v-col cols="9">
+                                    <v-card-title class="border justify-center py-1 px-0">
                                         {{ article.book.title }}
                                     </v-card-title>
                                 </v-col>
