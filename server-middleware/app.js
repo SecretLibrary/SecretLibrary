243fa6ce83aac36331d037316836a8c31d2a2152
eggplantiny <eggplantiny@gmail.com>
2021-11-03 22:35:29 +0900

    update express session config

--- server-middleware/app.js
+++ server-middleware/app.js
@@ -10,17 +10,20 @@ const app = express()
 
 const secret = process.env.SESSION_KEY
 
+app.set('trust proxy', 1)
+
 //  Express Settings
 app.use(cors({
     origin: [
         'http://localhost:3000',
         'https://www.secretlibrary.net'
-    ]
-    // methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-    // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
-    // exposedHeaders: 'Authorization',
-    // credentials: true
+    ],
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTION'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+    exposedHeaders: 'Authorization',
+    credentials: true
 }))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
@@ -28,7 +31,7 @@ app.use(session({
     proxy: true,
     resave: false,
     saveUninitialized: false,
-    cookie: { expires: 14 * 24 * 60 * 60 * 1000 },
+    cookie: { expires: 14 * 24 * 60 * 60 * 1000, secure: true },
     store: new MemoryStore()
 }))
 
